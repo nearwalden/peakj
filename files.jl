29f8dcd76dbe925dc229d11c612b111201c6fabf
David Douglas <nearwalden@gmail.com>
2023-08-20 22:25:34 -0600

    reimpelemented with dictionaries

--- files.jl
+++ files.jl
@@ -5,58 +5,20 @@
 # basepath of the data directories
 data_basepath = "/Users/dd/pCloud Drive/data/population/"
 
-# files
-struct Files
-    pop_data
-end
-
-# Pop_Collection
-struct PopCollection
-    basepath
-    vals
-end
-
-# collections
-struct Collections
-    global_pop::PopCollection
-    all_pop::PopCollection
-    country_pop::PopCollection
-end
-
-# types of data
-struct DataTypes
-    files
-    collections
-end
-
-# basic struct for describing a DataSource
-struct DataSource
-    path
-    bsaepath
-    data::DataTypes
-end
-
-
-
-
-
-
 # Gates foundation population data
-# BMGF = {'path': 'gatesfoundation/',
-#         'basepath': DATA_BASEPATH,
-#         'data': 
-#             {'files': 
-#                 {'pop_data': 'IHME_POP_2017_2100_POP_BOTH_SEX_ALL_AGE_Y2020M05D01.csv'
-#                 },
-#             'collections':
-#                 {'global_pop': {
-#                     'basepath': 'IHME_POP_2017_2100_GLOBAL_{}.csv',
-#                     'vals': ['SLOWER', 'REFERENCE', 'FASTER', 'SDG']
-#                     },
-#                 'country_pop': {
-#                     'basepath': 'IHME_POP_2017_2100_COUNTRY_{}.csv',
-#                     'vals': ['SLOWER', 'REFERENCE', 'FASTER', 'SDG']
-#                     }
-#                 }
-#             }
-# }
+bmgf = Dict(
+    "path" => "gatesfoundation/",
+    "files" => Dict(
+        "pop_data" => "IHME_POP_2017_2100_POP_BOTH_SEX_ALL_AGE_Y2020M05D01.csv"
+    ),
+    "collections" => Dict(
+        "global_pop" => Dict(
+            "basepath" => "IHME_POP_2017_2100_GLOBAL_{}.csv",
+            "vals" => ["SLOWER", "REFERENCE", "FASTER", "SDG"]
+        ),
+        "country_pop" => Dict(
+            "basepath" => "IHME_POP_2017_2100_COUNTRY_{}.csv",
+            "vals" => ["SLOWER", "REFERENCE", "FASTER", "SDG"]
+        )
+    )
+)
